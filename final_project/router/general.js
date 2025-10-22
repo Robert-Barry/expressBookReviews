6e160fd148088888a8fcf7ea607191781c329b57
Robert Barry <rb4080@gmail.com>
2025-10-22 12:02:31 -0400

    Add code to retrieve book information by title. Add helper function to include the isbn in book information sent to user

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,9 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const createBookObject = (book, isbn) => {
+    return { "isbn": isbn, "author": book["author"], "title": book["title"], "reviews": book["reviews"] }
+}
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -21,7 +24,8 @@ public_users.get('/isbn/:isbn',function (req, res) {
     const book = books[isbn];
 
     if (book) {
-        res.send(book);
+        const bookObject = createBookObject(books[isbn], isbn);
+        res.send(JSON.stringify(bookObject, null, 4));
     } else {
         res.send(`No book listed with ISBN: ${isbn}`);
     }
@@ -29,15 +33,38 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let authorBooks = [];
+    const author = req.params.author;
+    for (isbn in books) {
+        if (books[isbn]["author"] === author) {
+            let booksObject = createBookObject(books[isbn], isbn);
+            authorBooks.push(booksObject);
+        }
+    }
+    if (authorBooks.length > 0) {
+        res.send(JSON.stringify(authorBooks, null, 4));
+    } else {
+        res.send(`${author} not found`);
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let bookObject;
+    for (isbn in books) {
+        if (books[isbn]["title"] === title) {
+            bookObject = createBookObject(books[isbn], isbn);
+        }
+    }
+
+    if (bookObject != undefined) {
+        res.send(JSON.stringify(bookObject, null, 4));
+    } else {
+        res.send(`${title} not found`);
+    }
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
