4a5508904340d89073b5bcd9c06e9990a9b7e297
Robert Barry <rb4080@gmail.com>
2025-10-22 18:41:00 -0400

    Fix spelling error in index.js

--- final_project/index.js
+++ final_project/index.js
@@ -12,8 +12,8 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
     // Check if the user logged in and has a valid access token
-    if (req.session.authroization) {
-        let token = req.session.authroization['accessToken'];
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
 
         // Verify JWT Token
         jwt.verify(token, "access", (err, user) => {
