fb8a2db675c54e07a57f18c01d420f491e822874
Robert Barry <rb4080@gmail.com>
2025-10-22 18:36:10 -0400

    Fix issue with saving the password when registering a new user

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,7 +32,7 @@ public_users.post("/register", (req,res) => {
     if (username && password) {
         if (!doesExist(username)) {
             // Add the new user to the users array
-            users.push({ "username": username, password: "password" });
+            users.push({ "username": username, "password": password });
             return res.status(200).json({ message: "User successfully registered and can log in." });
         } else {
             return res.status(404).json({ message: "User already exists!" });
