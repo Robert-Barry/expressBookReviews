48a11b3cc9913c86f947c66094a5a8100dde9853
Robert Barry <rb4080@gmail.com>
2025-10-22 12:17:55 -0400

    Add JSON.stringify to code to return reviews.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -70,7 +70,7 @@ public_users.get('/title/:title',function (req, res) {
 public_users.get('/review/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     if (books[isbn]) {
-        res.send(books[isbn]["reviews"]);
+        res.send(JSON.stringify(books[isbn]["reviews"], null, 4));
     } else {
         res.send(`Book with ISBN ${isbn} not found`);
     }
